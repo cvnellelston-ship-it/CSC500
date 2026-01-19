f6af834e1cc968f58935f52728ec961ef2272605
Carmen <c.vnell.elston@gmail.com>
2026-01-18 22:02:39 -0500

    Add files via upload

---
+++ csc500wk1.py
@@ -0,0 +1,22 @@
+#Carmen V. Elston CSC500 Week 1
+#Part 1 addition and subtraction
+num1 = float(input('Enter the first number:'))
+num2 = float(input('Enter the second number:'))
+print()
+
+addition = num1 + num2
+subtraction = num1 - num2
+
+print('Part 1')
+
+print('The sum is', addition)
+print('The difference is', subtraction)
+print()
+
+#Part 2 Multiplication
+print('Part 2')
+
+multiplication= num1 * num2
+
+print('The multiplication is', multiplication)
+
