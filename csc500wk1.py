70c38766dde05e56f84f796830121692e026f842
Carmen <c.vnell.elston@gmail.com>
2026-01-18 22:50:11 -0500

    Add files via upload

--- csc500wk1.py
+++ csc500wk1.py
@@ -1,4 +1,4 @@
-#Part 1 addition and subtraction
+#Part 1 Addition and Subtraction
 
 num1 = float(input('Enter the first number:'))
 num2 = float(input('Enter the second number:'))
@@ -11,10 +11,12 @@ print('The sum is', addition)
 print('The difference is', subtraction)
 print()
 
-#Part 2 Multiplication
+#Part 2 Multiplication and Division
 print('Part 2')
 
 multiplication= num1 * num2
+quotient = num1 / num2
 
 print('The product is', multiplication)
+print('The quotient is', quotient)
 
