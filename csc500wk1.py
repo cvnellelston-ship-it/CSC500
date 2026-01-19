958c4b947f2c6c0c241e540ec66ebb841f63d950
Carmen <c.vnell.elston@gmail.com>
2026-01-18 22:53:06 -0500

    Add files via upload

--- csc500wk1.py
+++ csc500wk1.py
@@ -3,6 +3,7 @@
 num1 = float(input('Enter the first number:'))
 num2 = float(input('Enter the second number:'))
 print()
+
 addition = num1 + num2
 subtraction = num1 - num2
 
@@ -12,11 +13,11 @@ print('The difference is', subtraction)
 print()
 
 #Part 2 Multiplication and Division
-print('Part 2')
 
 multiplication= num1 * num2
 quotient = num1 / num2
 
+print('Part 2')
 print('The product is', multiplication)
 print('The quotient is', quotient)
 
